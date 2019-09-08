569184841e8f039fb16ef35b4a473c143d4a1fbc
Cyril Verluise <cyril.verluise@gmail.com>
2019-09-08 17:44:05 +0200

    Add v0.1 pkg

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="pnpl",
+    version="0.1",
+    packages=["pnpl"],
+    url="",
+    license="",
+    author="Cyril Verluise",
+    author_email="cyril.verluise@gmail.com",
+    description="",
+)
