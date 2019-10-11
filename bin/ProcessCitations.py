301d9815f4d6f8ee3276fdef4a692f6397101354
Cyril Verluise <cyril.verluise@gmail.com>
2019-10-11 11:58:32 +0200

    Add full-text flavor

--- bin/ProcessCitations.py
+++ bin/ProcessCitations.py
@@ -3,22 +3,53 @@ import os
 
 import click
 
-from scicit.io import process_biblio_tls214
+from scicit.io import process_biblio_tls214, process_full_text
 
 
 @click.command()
-@click.argument("path", required=False, type=str)
-def main(path):
+@click.option(
+    "--path", type=str, help="Path of folder containing files to be processed"
+)
+@click.option(
+    "--pattern",
+    type=str,
+    help="Sequence of characters which will be used to filter "
+    "files of interest. E.g. 'sub', 'us_'",
+)
+@click.option(
+    "--flavor",
+    type=str,
+    help="Refers to the type of files to be processed. Currently "
+    "supported: 'tls_214' and 'full-text'",
+)
+@click.option(
+    "--max_workers",
+    type=str,
+    help="Maximum number of threads running in parallel'",
+)
+def main(path, pattern, flavor, max_workers):
     """
-    Process all files in <path> (if "sub" in file name).
-    MultiThreaded, max_workers=10 (restricted by nbr of engines supported by grobid service)
+    Process all files in <path> (if "<pattern>" in file name).
+    MultiThreaded, <max_workers> restricted by nbr of engines supported by grobid service
     :param path: str
+    :param pattern: str
+    :param flavor: str
+    :param max_workers: int
     :return:
     """
-    input_files = [path + file for file in os.listdir(path) if "sub" in file]
+    assert flavor in ["tls214", "full-text"]
+    input_files = [path + file for file in os.listdir(path) if pattern in file]
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        executor.map(process_biblio_tls214, input_files)
+    if flavor == "tls214":
+        with concurrent.futures.ThreadPoolExecutor(
+            max_workers=max_workers
+        ) as executor:
+            executor.map(process_biblio_tls214, input_files)
+    else:
+        with concurrent.futures.ThreadPoolExecutor(
+            max_workers=max_workers
+        ) as executor:
+            executor.map(process_full_text, input_files)
 
 
 if __name__ == "__main__":
