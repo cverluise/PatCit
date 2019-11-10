59425e24a3ace4a10f90e007f34804c7f2aa0995
Cyril Verluise <cyril.verluise@gmail.com>
2019-11-11 00:29:42 +0100

    :tada: Add validation schema for npl_citation

---
+++ scicit/validation/schema.py
@@ -0,0 +1,40 @@
+npl_citation_schema = {
+    "type": "object",
+    "properties": {
+        "npl_publn_id": {"type": "number"},
+        "DOI": {"type": "string"},
+        "ISSN": {"type": "string"},
+        "ISSNe": {"type": "string"},
+        "PMCID": {"type": "string"},
+        "PMID": {"type": "string"},
+        "authors": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "first": {"type": "string"},
+                    "middle": {"type": "string"},
+                    "surname": {"type": "string"},
+                    "genname": {"type": "string"},
+                },
+            },
+        },
+        "target": {"type": "string"},
+        "title_j": {"type": "string"},
+        "title_abbrev_j": {"type": "string"},
+        "title_m": {"type": "string"},
+        "title_main_m": {"type": "string"},
+        "title_main_a": {"type": "string"},
+        "year": {"type": "number"},
+        "issue": {"type": "number"},
+        "volume": {"type": "number"},
+        "from": {"type": "number"},
+        "to": {"type": "number"},
+        # 'page': {"type": "string"},
+        # 'type': {"type": "string"},
+        # 'unit': {"type":},
+        # 'when': {"type": "string"}
+        # 'idno': {"type":},
+    },
+    "required": ["npl_publn_id"],
+}
