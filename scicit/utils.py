18a8441baf197ce611bbb9fa197240983ea593cd
Cyril Verluise <cyril.verluise@gmail.com>
2019-11-18 19:30:01 +0100

    :tada: add utils submodule. Mainly for bq so far

---
+++ scicit/utils.py
@@ -0,0 +1,21 @@
+from scicit.config import Config
+
+
+def str_to_bq_ref(s):
+    """
+    Return a bq.TableRef from a string like path (e.g npl-parsing.patcit.raw)
+    :param s: str
+    :return: bq.Tableref
+    """
+    project_id, dataset_id, table_id = s.split(".")
+    config = Config(project_id=project_id, dataset_id=dataset_id)
+    return config.table_ref(table_id)
+
+
+def ref_to_bq_path(ref):
+    """
+    Return a string like path from a bq.TableRef object
+    :param ref: bq.Tableref
+    :return: str
+    """
+    return ".".join(ref._key())
