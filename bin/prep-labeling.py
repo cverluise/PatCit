75aaa0e1cb69e95975d874478e99a5f6b7feab98
Cyril Verluise <cyril.verluise@gmail.com>
2020-03-04 01:16:48 +0100

    :tada: Add filter non bibl ref feature

--- bin/prep-labeling.py
+++ bin/prep-labeling.py
@@ -2,10 +2,16 @@ import os
 from glob import glob
 
 import pandas as pd
+import spacy
 import typer
 
 
-def main(path: str, sample_size: int = None):
+def main(
+    path: str,
+    sample_size: int = None,
+    filter_bibl_ref: bool = False,
+    spacy_model: str = None,
+):
     """
     Prep GBQ extracts for doccano
     """
@@ -18,10 +24,22 @@ def main(path: str, sample_size: int = None):
             fout = os.path.join(
                 os.path.dirname(file), f"sample-prep-{os.path.basename(file)}"
             )
-
-        df = df.set_index("npl_biblio")
-        df.index.name = "text"
-        df.to_csv(fout)
+        if filter_bibl_ref:
+            # we filter out non bibliographical ref npls - based on spacy_model textcat
+            fout = fout.replace(".csv", ".txt")
+            nlp = spacy.load(spacy_model)
+            texts = df["npl_biblio"].values
+            docs = list(nlp.pipe(texts))
+            cats = [doc.cats for doc in docs]
+            bibl_ref = [
+                True if cat["BIBLIOGRAPHICAL_REFERENCE"] > 0.5 else False
+                for cat in cats
+            ]
+            df.loc[bibl_ref]["npl_biblio"].to_string(fout, index=False, header=False)
+        else:
+            df = df.set_index("npl_biblio")
+            df.index.name = "text"
+            df.to_csv(fout)
         typer.secho(f"{fout} successfully saved.", fg=typer.colors.GREEN)
 
 
