68b440de6973cf019c7050fdf5c1d80b21ac4c88
Cyril Verluise <cyril.verluise@gmail.com>
2020-05-11 18:12:16 +0200

    :fire::tada: merge npl_get-lang and npl_categorize to npl + add to patcit-cli

--- cli/patcit-cli.py
+++ cli/patcit-cli.py
@@ -1,9 +1,11 @@
 import patcit_bq
+import patcit_npl
 import typer
 
 app = typer.Typer()
 
 app.add_typer(patcit_bq.app, name="bq")
+app.add_typer(patcit_npl.app, name="npl")
 
 if __name__ == "__main__":
     app()
