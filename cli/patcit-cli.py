9dbb353f86bfec95a6f8b6d7082f852648945ba8
Cyril Verluise <cyril.verluise@gmail.com>
2020-05-11 19:21:17 +0200

    :tada: add schema binding

--- cli/patcit-cli.py
+++ cli/patcit-cli.py
@@ -1,11 +1,13 @@
 import patcit_bq
 import patcit_npl
+import patcit_schema
 import typer
 
 app = typer.Typer()
 
 app.add_typer(patcit_bq.app, name="bq")
 app.add_typer(patcit_npl.app, name="npl")
+app.add_typer(patcit_schema.app, name="schema")
 
 if __name__ == "__main__":
     app()
