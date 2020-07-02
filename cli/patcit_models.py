1cc3926576c4a523378a73cf9a264d5662542dbe
Cyril Verluise <cyril.verluise@gmail.com>
2020-07-02 14:39:05 +0200

    :tada: add cli for adding custom pipeline urls_matcher to spaCy model

--- cli/patcit_models.py
+++ cli/patcit_models.py
@@ -1,12 +1,18 @@
 import patcit_models_data
 import patcit_models_evaluate
 import patcit_models_process
+
+import patcit_models_add
+import patcit_models_data
+import patcit_models_evaluate
 import patcit_models_train
+import patcit_models_process
 import patcit_models_finetune
 import typer
 
 app = typer.Typer()
 
+app.add_typer(patcit_models_add.app, name="add")
 app.add_typer(patcit_models_data.app, name="data")
 app.add_typer(patcit_models_evaluate.app, name="evaluate")
 app.add_typer(patcit_models_train.app, name="train")
