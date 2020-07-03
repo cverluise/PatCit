d70fb6ec46695073365103e14c849ac695821db1
Cyril Verluise <cyril.verluise@gmail.com>
2020-07-03 11:38:03 +0200

    :tada: add date parser util. might become spaCy pipeline custom component in the future

--- patcit/utils.py
+++ patcit/utils.py
@@ -1,4 +1,7 @@
 from patcit.config import Config
+import dateparser
+
+TODAY = int(dateparser.parse("today").strftime("%Y%m%d"))
 
 
 def str_to_bq_ref(s):
@@ -19,3 +22,23 @@ def ref_to_bq_path(ref):
     :return: str
     """
     return ".".join(ref._key())
+
+
+def parse_date(s):
+    """
+    Return the yyyymmdd format of the natural language date in S
+    :param s: str, natural language date (e.g. '19 juin 1991')
+    :return: str
+    """
+    try:
+        date = dateparser.parse(s)
+    except OverflowError:
+        date = 0
+    if date:
+        date = int(date.strftime("%Y%m%d"))
+        if date > TODAY:
+            #  handle cases where dateparser parses futuristic dates, which should not be the case
+            date = 0
+    else:  # default when dateparser.parse fails
+        date = 0
+    return date
