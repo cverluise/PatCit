06ea155c04a275918798f9d92b734630cd8e81e5
Cyril Verluise <cyril.verluise@gmail.com>
2020-07-03 11:39:27 +0200

    :bug: some minor fixes

--- cli/patcit_models_add.py
+++ cli/patcit_models_add.py
@@ -1,30 +1,11 @@
 import re
+from urllib.parse import urlparse
 
 import spacy
 import typer
 from spacy.language import Doc
 
 app = typer.Typer()
-
-
-# class RESTCountriesComponent(object):
-#     name = 'countries'
-#     def __init__(self, nlp, label='GPE'):
-#         self.countries = [u'MyCountry', u'MyOtherCountry']
-#         self.label = nlp.vocab.strings[label]
-#         patterns = [nlp(c) for c in self.countries]
-#         self.matcher = PhraseMatcher(nlp.vocab)
-#         self.matcher.add('COUNTRIES', None, *patterns)
-#     def __call__(self, doc):
-#         matches = self.matcher(doc)
-#         spans = []
-#         for _, start, end in matches:
-#             entity = Span(doc, start, end, label=self.label)
-#             spans.append(entity)
-#         doc.ents = list(doc.ents) + spans
-#         for span in spans:
-#             span.merge()
-#         return doc
 
 
 class UrlsMatcher(object):
@@ -35,20 +16,43 @@ class UrlsMatcher(object):
 
     def __call__(self, doc):
         # http://www.noah.org/wiki/RegEx_Python
-        expression = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
-        urls = re.findall(expression, doc.text)
+        expression = (
+            "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F]["
+            "0-9a-fA-F]))+"
+        )
         Doc.set_extension("urls", default=True, force=True)
         doc._.urls = re.findall(expression, doc.text)
         return doc
 
 
-# def urls_matcher_(doc):
+class UrlsHostname(object):
+    name = "urls_hostname"
+
+    def __init__(self, nlp):
+        pass
+
+    def __call__(self, doc):
+        # https://docs.python.org/3/library/urllib.parse.html
+        Doc.set_extension("hostnames", default=True, force=True)
+        hostnames = []
+        for url in doc._.urls:
+            try:
+                hostnames += [urlparse(url).hostname]
+            except ValueError:
+                pass
+        doc._.hostnames = hostnames
+        return doc
 
 
 @app.command()
-def urls_matcher(model: str, dest: str = None):
-    """Add custom component matching urls based on regex"""
+def url_components(model: str, dest: str = None):
+    """Add custom component matching and parsing urls based on regex"""
     nlp = spacy.blank(model) if len(model) == 2 else spacy.load(model)
     nlp.add_pipe(UrlsMatcher, last=True)
+    nlp.add_pipe(UrlsHostname, last=True)
     if dest:
         nlp.to_disk(dest)
+
+
+if __name__ == "__main__":
+    app()
