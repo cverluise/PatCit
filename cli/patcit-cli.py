09d04c4f2250fb8e5a2569d4a8e9c7e10678cf2d
Cyril Verluise <cyril.verluise@gmail.com>
2020-10-09 00:56:13 +0200

    :bug: fix bug missing file :fire: in import statement

--- cli/patcit-cli.py
+++ cli/patcit-cli.py
@@ -1,5 +1,4 @@
 import patcit_bq
-import patcit_npl
 import patcit_schema
 import patcit_serialize
 import patcit_grobid
@@ -9,7 +8,6 @@ import typer
 app = typer.Typer()
 
 app.add_typer(patcit_bq.app, name="bq")
-app.add_typer(patcit_npl.app, name="npl")
 app.add_typer(patcit_schema.app, name="schema")
 app.add_typer(patcit_serialize.app, name="serialize")
 app.add_typer(patcit_grobid.app, name="grobid")
