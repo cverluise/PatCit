61477becfe70eb9ed9153280ecf219cadd66c5e3
Cyril Verluise <cyril.verluise@gmail.com>
2020-10-11 23:02:26 +0200

    :fire: remove deprecated building recipe/method

--- cli/patcit_bq.py
+++ cli/patcit_bq.py
@@ -1,12 +1,10 @@
 import patcit_bq_create
-import patcit_bq_enrich
 import patcit_bq_export
 import typer
 
 app = typer.Typer()
 
 app.add_typer(patcit_bq_create.app, name="create")
-app.add_typer(patcit_bq_enrich.app, name="enrich")
 app.add_typer(patcit_bq_export.app, name="export")
 
 if __name__ == "__main__":
